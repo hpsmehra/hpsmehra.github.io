7b3199c82ab529f26e02615971ccd2e2e3d64e07
hpsmehra <hpsmehra@gmail.com>
2016-07-07 23:59:39 +0530

    new js
    new js

---
+++ js/backtotop.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(function () {
+    $('body').append('<div id="back-top"><a href="#top"><i class="fa fa-chevron-up"></i></a></div>')
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('#back-top').fadeIn();
+        } else {
+            $('#back-top').fadeOut();
+        }
+    });
+
+    // scroll body to 0px on click
+    $('#back-top a').click(function (e) {
+        e.preventDefault();
+        $('body,html').animate({
+            scrollTop: 0
+        }, 400);
+        return false;
+    });
+});
